922a5b507ae24c1fa166289a362656fcfa887bbc
alawaree <60848161+alawaree@users.noreply.github.com>
2023-02-21 06:52:34 +0100

    Preparing experiment

--- run_HopfieldNetwork_hits.py
+++ run_HopfieldNetwork_hits.py
@@ -1010,13 +1010,13 @@ if __name__ == "__main__":
 #9,10: "Samples_3265_to_3719_hits", "Samples_3726_to_8666_hits"
 
 
-samples_dataset_minibias = ["Samples_1550_to_1812_hits","Samples_1819_to_2119_hits"]
+samples_dataset_minibias = ["Samples_2553_to_2851_hits"]
 
 for index, sample in enumerate(samples_dataset_minibias):
     save_experiment(
         "results_minibias_samples_hits",
-        f"Test of the Hopfield network on the {index+5}th sample minibias dataset",
-        f"Upgraded network - Best Configuration test on 5 events from the {index+5}th sample of minibias dataset ({sample})",
+        f"Test of the Hopfield network on the {index+7}th sample minibias dataset",
+        f"Upgraded network - Best Configuration test on 5 events from the {index+7}th sample of minibias dataset ({sample})",
         parameters,
         f"/datasets/samples/minibias_samples_hits/{sample}/velo_event_",
         10,
